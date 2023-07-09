26a65d1ce01895cd6ee15096495e3a565f0aa50e
Asaduzzaman_mac <samrat.asad@gmail.com>
2023-07-09 20:47:17 +0600

    init

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:chatapp_fluttebase/shared/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  if(kIsWeb){
+    await Firebase.initializeApp(
+      options: FirebaseOptions(apiKey: Constants.apiKey, appId: Constants.appId,
+        messagingSenderId: Constants.messagingSenderId, projectId: Constants
+            .projectId,)
+    );
+  } else{
+
+    await Firebase.initializeApp();
+  }
+
+runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId =  "1:115170808137:web:998e04f36b79f394f4ca98";
+  static String apiKey = "AIzaSyCV2PsSsJrX8L_CQ1_hWOWyZl86ZBpXZJ4" ;
+  static String messagingSenderId = "115170808137";
+  static String projectId = "chatappflutter-b6153";
+}
