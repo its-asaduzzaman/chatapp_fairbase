7baf961ee03fcc919726167bab7e4bcbfa813cf9
Asaduzzaman_mac <samrat.asad@gmail.com>
2023-07-09 21:51:11 +0600

    SharedPreference logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:chatapp_fluttebase/helper/helper_function.dart';
+import 'package:chatapp_fluttebase/pages/auth/login_page.dart';
+import 'package:chatapp_fluttebase/pages/home_page.dart';
 import 'package:chatapp_fluttebase/shared/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -18,11 +21,33 @@ void main()async{
 runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isSignedIn = false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getUserLoggedInStatus();
+  }
+  getUserLoggedInStatus()async{
+    await HelperFunction.getUserLoggedInStatus().then((value) {
+      if (value !=null){
+        _isSignedIn = value;
+      }
+    });
+  }
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:_isSignedIn? HomePage(): LoginPage(),
+    );
   }
 }
